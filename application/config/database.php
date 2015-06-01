d4b5766455c48cd2d49459a1bd9ad8c4b3ce1120
kevinvanzyl <kevinvanzyl@users.noreply.github.com>
2015-06-02 00:40:45 +0200

    change config

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'k3v1n';
-$db['default']['database'] = 'uj_xrd';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = 'xrd_portal';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
